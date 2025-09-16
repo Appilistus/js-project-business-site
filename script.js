fce9aef077d50efb4378d5421079b752bdd5bbdc
Asako Kanno <asakok@AsakonoMacBook-Air.local>
2025-09-16 21:44:26 +0200

    add hamburger btn, still need some styling

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const btn = document.querySelector(".btn");
+const mobileNav = document.querySelector(".mobile-navbar");
+
 btn.addEventListener("click", () => {
     btn.classList.toggle("active");
+    mobileNav.classList.toggle("active");
+});
+
+//close button when click menu
+document.querySelectorAll(".mobile-navbar a").forEach(link => {
+    link.addEventListener("click", () => {
+        btn.classList.remove("active");
+        mobileNav.classList.remove("active");
+    });
 });

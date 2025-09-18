add54d76e4a4c0267ab699a0f87f0453a27570d3
Asako Kanno <asakok@AsakonoMacBook-Air.local>
2025-09-18 11:16:34 +0200

    style hamburger menu

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const btn = document.querySelector(".btn");
 const mobileNav = document.querySelector(".mobile-navbar");
+const overlay = document.getElementById("overlay");
 
 btn.addEventListener("click", () => {
     btn.classList.toggle("active");
     mobileNav.classList.toggle("active");
+    overlay.classList.toggle("active");
 });
 
 //close button when click menu
@@ -11,5 +13,6 @@ document.querySelectorAll(".mobile-navbar a").forEach(link => {
     link.addEventListener("click", () => {
         btn.classList.remove("active");
         mobileNav.classList.remove("active");
+        overlay.classList.remove("active");
     });
 });
